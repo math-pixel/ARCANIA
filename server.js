71aba7fbd0b36d3c1e250c78e99e11d9081e4011
anthony74742 <antho230104@gmail.com>
2023-11-10 16:27:13 +0100

    affichage

--- server.js
+++ server.js
@@ -4,15 +4,19 @@ const { join } = require('node:path');
 const { Server } = require('socket.io');
 
 const app = express();
+app.use('/medias', express.static(join(__dirname, 'medias')))
+app.use('/sources', express.static(join(__dirname, 'sources')))
 const server = createServer(app);
 const io = new Server(server);
 
 app.get('/projo', (req, res) => {
-  res.sendFile(join(__dirname, 'projo.html'));
+  res.sendFile(join(__dirname, 'sources/projo/projo.html'));
 });
 
 io.on('connection', (socket) => {
   console.log('a user connected');
+
+  
 });
 
 server.listen(3000, () => {
