68205b5299165868466d2557d0a9445451359bb4
anthony74742 <antho230104@gmail.com>
2023-11-20 16:28:28 +0100

    update(loading): add spell loading

--- src/services/projo/projo.js
+++ src/services/projo/projo.js
@@ -1,53 +1,58 @@
+
+
+
 document.getElementById("introButton").addEventListener("click", () => {
     startVideo()
-  })
-  
-  function startVideo() {
+})
+
+function startVideo() {
     let introDiv = document.getElementById("intro")
     introDiv.parentNode.removeChild(introDiv);
     let videoIntro = document.getElementById("vidIntro")
     console.log(videoIntro)
     videoIntro.play()
     setTimeout(() => {
-      videoIntro.parentNode.removeChild(videoIntro);
-    }, 30000)
-  }
-  
-  let spells = []; // Declare the variable to hold the preloaded data
-  
-  function preloadSpells() {
-    fetch('/json/spells.json')
-      .then((response) => response.json())
-      .then((json) => {
-          spells = json["spells"]; // Assign the JSON data to the spells variable
-          // You can perform additional actions here if needed
-      })
-      .catch((error) => {
-          console.error('Error preloading spells:', error);
-      });
-  }
-  
-  // Call the preloadSpells function when your application loads
-  window.onload = preloadSpells;
-  const socket = io();
-  
-  
-  let player1 = {
+        videoIntro.parentNode.removeChild(videoIntro);
+    }, 2000)
+}
+
+let spells = []; // Declare the variable to hold the preloaded data
+
+function preloadSpells() {
+fetch('/json/spells.json')
+    .then((response) => response.json())
+    .then((json) => {
+        spells = json["spells"]; // Assign the JSON data to the spells variable
+        // You can perform additional actions here if needed
+    })
+    .catch((error) => {
+        console.error('Error preloading spells:', error);
+    });
+}
+
+// Call the preloadSpells function when your application loads
+window.onload = preloadSpells;
+const socket = io();
+
+
+let player1 = {
     name: "player1",
     life: 100,
-    mana: 0
-  }
-  
-  let player2 = {
+    mana: 0,
+    loading: false
+}
+
+let player2 = {
     name: "player2",
     life: 100,
-    mana: 0
-  }
-  
-  let parentVideo = document.getElementById("videoDiv")
-  let parentAudio = document.getElementById("audioDiv")
-  
-  function displaySpell(source, player) {
+    mana: 0,
+    loading: false
+}
+
+let parentVideo = document.getElementById("videoDiv")
+let parentAudio = document.getElementById("audioDiv")
+
+function displaySpell(source, player) {
     let audio = document.createElement('audio')
     audio.src = source.audioSrc
     audio.preload = "auto"
@@ -62,82 +67,93 @@ document.getElementById("introButton").addEventListener("click", () => {
     video.type = "video/webp"
     video.preload = "auto"
     if (player.name == "player2") {
-      video.style.transform = "rotate(180deg)"
+        video.style.transform = "rotate(180deg)"
     }
-    video.addEventListener("ended", function() {
-      video.parentNode.removeChild(video);
-      life(player, source)
-    })
+    if (!source.name.includes("loading")) {
+        player.loading = false
+        video.addEventListener("ended", function() {
+            video.parentNode.removeChild(video);
+            life(player, source)
+        })
+    } else {
+        video.loop = true
+        player.loading = true
+        setInterval((video, player) => {
+            if (!player.loading) {
+                video.parentNode.removeChild(video);
+            }
+        }, 100)
+    }
     audio.addEventListener("ended", function() {
         audio.parentNode.removeChild(audio);
     })
     parentAudio.appendChild(audio)
     parentVideo.appendChild(video)
-  }
-  
-  function detectSpell(name, player) {
+}
+
+function detectSpell(name, player) {
     for (const spell of spells) {
-      if (spell.name == name) {
+        if (spell.name == name) {
         displaySpell(spell, player)
-      }
+        }
     }
     return null
-  }
-  
-  function life(player, lifeValue) {
+}
+
+function life(player, lifeValue) {
     if (lifeValue["damage"] != 0) {
-      if (player["life"] - lifeValue["damage"] > 0) {
+        if (player["life"] - lifeValue["damage"] > 0) {
         player["life"] -= lifeValue["damage"]
         console.log(player["life"])
         updateLife(player, "shot")
-      } else {
+        } else {
         console.log(player + ": dead !!!")
         player["life"] = 0
         console.log(player["life"])
         updateLife(player, "shot")
-      }
+        }
     } else {
-      if (player["life"] + lifeValue["heal"] > 100) {
+        if (player["life"] + lifeValue["heal"] > 100) {
         player["life"] = 100
         console.log(player["life"])
         updateLife(player, "heal")
-      } else {
+        } else {
         player["life"] += lifeValue["heal"]
         console.log(player["life"])
         updateLife(player, "heal")
-      }
+        }
     }
-  }
-  
-  function updateLife(player, state) {
+}
+
+function updateLife(player, state) {
     console.log(player)
     let lifeDiv;
     if (player == player1 && state == "shot") {
-      lifeDiv = document.getElementById("infoLife2")
+        lifeDiv = document.getElementById("infoLife2")
     } else if (player == player1 && state == "heal") {
-      lifeDiv = document.getElementById("infoLife1")
+        lifeDiv = document.getElementById("infoLife1")
     } else if (player == player2 && state == "shot") {
-      lifeDiv = document.getElementById("infoLife1")
+        lifeDiv = document.getElementById("infoLife1")
     } else {
-      lifeDiv = document.getElementById("infoLife2")
+        lifeDiv = document.getElementById("infoLife2")
     }
     lifeDiv.style.width = player["life"] + "%"
-  }
-  
-  function mana(player, state) {
-    if (state == "gain") {
-      if (player["mana"] + 10 < 100) {
-        player["mana"] += 10
-      } else {
-        player["mana"] = 100
-        // Launch event for ulti
-  
-      }
+}
+
+function mana(player, state) {
+if (state == "gain") {
+    if (player["mana"] + 10 < 100) {
+    player["mana"] += 10
     } else {
-      // Here when ulti is launch
-      player["mana"] = 0
+    player["mana"] = 100
+    // Launch event for ulti
+
     }
-  }
+} else {
+    // Here when ulti is launch
+    player["mana"] = 0
+}
+}
   
 
 /* -------------------------------------------------------------------------- */
@@ -151,6 +167,14 @@ socket.on("player1", (spell) => {
 
         case "line":
             detectSpell("line", player1)
+            break;
+            
+        case "circle_loading":
+            detectSpell("circle_loading", player1)
+            break;
+
+        case "line_loading":
+            detectSpell("line_loading", player1)
             break;
     
         default:
@@ -167,6 +191,14 @@ socket.on("player2", (spell) => {
             detectSpell("line", player2)
             break;
     
+        case "circle_loading":
+            detectSpell("circle_loading", player2)
+            break;
+
+        case "line_loading":
+            detectSpell("line_loading", player2)
+            break;
+
         default:
             break;
     }
