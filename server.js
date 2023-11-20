2cd3a245b86fffe5252ecd923e311f26279e44d7
math-pixel <59537610+math-pixel@users.noreply.github.com>
2023-11-20 09:14:48 +0100

    set up new root directory

--- server.js
+++ server.js
@@ -4,22 +4,38 @@ const { join } = require('node:path');
 const { Server } = require('socket.io');
 const path = require('path')
 
+/* -------------------------------------------------------------------------- */
+/*                         Config/Init Express server                         */
+/* -------------------------------------------------------------------------- */
 const app = express();
-app.use('/medias', express.static(join(__dirname, 'medias')))
-app.use('/sources', express.static(join(__dirname, 'sources')))
-app.use('/phone', express.static(path.join(__dirname, 'phone')))
-app.use('/model', express.static(path.join(__dirname, 'model')))
+app.use('/medias', express.static(join(__dirname, 'src/medias')))
+app.use('/services', express.static(path.join(__dirname, 'src/services')))
+app.use('/phone', express.static(path.join(__dirname, 'src/services/phone')))
+app.use('/projo', express.static(path.join(__dirname, 'src/services/projo')))
+app.use('/json', express.static(path.join(__dirname, 'src/json')))
+app.use('/model', express.static(path.join(__dirname, 'src/services/models')))
+
+/* -------------------------------------------------------------------------- */
+/*                        Config/Init Websocket server                        */
+/* -------------------------------------------------------------------------- */
 const server = createServer(app);
 const io = new Server(server);
 
+/* -------------------------------------------------------------------------- */
+/*                               Routage Express                              */
+/* -------------------------------------------------------------------------- */
 app.get('/', (req, res) => {
-  res.sendFile(join(__dirname, 'phone/index.html'));
+  res.sendFile(join(__dirname, 'src/services/phone/index.html'));
 });
 
 app.get('/projo', (req, res) => {
-  res.sendFile(join(__dirname, 'sources/projo/projo.html'));
+  res.sendFile(join(__dirname, 'src/services/projo/projo.html'));
 });
 
+
+/* -------------------------------------------------------------------------- */
+/*                              Websocket Server                              */
+/* -------------------------------------------------------------------------- */
 io.on('connection', (socket) => {
   console.log('a user connected');
 
@@ -35,6 +51,7 @@ io.on('connection', (socket) => {
 
 });
 
+// Listen HTTP server
 server.listen(3000, () => {
-  console.log('server running at http://localhost:3000');
+  console.log('server HTML running on port 3000');
 });
